c1596e224b218b694e25ca18879ffef0ac9209b5
kferte <qwerteek@gmail.com>
2018-12-10 21:41:52 +0100

    Add box grid and mouse interaction.

--- src/GUI.java
+++ src/GUI.java
@@ -1,6 +1,14 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 public class GUI extends JFrame {
+
+    int spacing = 5;
+    public int mx = -1;
+    public int my = -1;
 
     public GUI(){
         this.setTitle("Minesweeper");
@@ -8,8 +16,75 @@ public class GUI extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
         this.setResizable(false);
+        Board board = new Board();
+        this.setContentPane(board);
+        Move move = new Move();
+        this.addMouseMotionListener(move);
+        Click click = new Click();
+        this.addMouseListener(click);
     }
 
     public void repaint() {
+        
+    }
+
+    public class Board extends JPanel {
+        public void paintComponent(Graphics g){
+            g.setColor(Color.DARK_GRAY);
+            g.fillRect(0, 0, 1280, 860);
+            for(int i = 0; i < 16; i++){
+                for(int j = 0; j < 9; j++){
+                    g.setColor(Color.GRAY);
+                    if(mx >= spacing + i * 80 && mx < i * 80 + 80 - spacing && my >= spacing + j * 80 + 80 + 26 && my < j * 80 + 26 + 80 + 80 - spacing){
+                        g.setColor(Color.RED);
+                    }
+                    g.fillRect(spacing + i * 80, spacing + j * 80 + 80, 80 - 2 * spacing, 80 - 2 * spacing);
+                }
+            }
+        }
+    }
+
+    public class Move implements MouseMotionListener {
+
+        @Override
+        public void mouseDragged(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseMoved(MouseEvent e) {
+            System.out.println("The mouse was moved");
+            mx = e.getX();
+            my = e.getY();
+            System.out.println(mx + " " + my);
+        }
+    }
+
+    public class Click implements MouseListener {
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            System.out.println("The mouse was clicked");
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+
+        }
     }
 }
